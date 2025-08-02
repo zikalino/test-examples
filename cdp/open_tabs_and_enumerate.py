e9c470168d93c57fb35851fda88310a1e721f4ed
Zim Kalinowski <zkalinowski@opera.com>
2025-08-02 11:06:02 +0800

    opening and enumerating tabs

---
+++ cdp/open_tabs_and_enumerate.py
@@ -0,0 +1,90 @@
+## demo.py
+
+import asyncio
+import websockets
+import json
+import time
+from pyppeteer import launch
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from settings import EXECUTABLE_LOCATION
+
+next_id = 1
+
+async def send_and_wait_for_response(websocket, session_id, method, params):
+    global next_id
+    # send message
+    message = { 'id': next_id,
+                'method': method,
+                'params': params
+              }
+    if session_id:
+        message['sessionId'] = session_id
+
+    await websocket.send(json.dumps(message))
+
+    # and await response, just dump all messages without appropriate id
+    response = {}
+    while (not 'id' in response) or (response['id'] != next_id):
+        response = json.loads(await websocket.recv())
+        print(f" ... received: ")
+        print(response)
+
+    next_id += 1
+    return response
+
+async def main():
+  global next_id
+  browser = await launch({"executablePath": EXECUTABLE_LOCATION})
+
+  async with websockets.connect(browser.wsEndpoint) as websocket:
+    print("CONNECTED....")
+
+    response = await send_and_wait_for_response(
+        websocket,
+        None,
+        'Target.createTarget',
+        {
+            'url': 'https://onet.pl'
+        }
+    )
+
+    response = await send_and_wait_for_response(
+        websocket,
+        None,
+        'Target.createTarget',
+        {
+            'url': 'https://gazeta.pl'
+        }
+    )
+
+
+    response = await send_and_wait_for_response(
+            websocket,
+            None,
+            'Target.getTargets',
+            {
+                'filter': [
+                    {
+                    "exclude": False,
+                    "type": "browser"
+                    },
+                    {
+                        "exclude": True,
+                        "type": "page"
+                    },
+                    {
+                        "exclude": False
+                    }
+                ]
+            })
+
+    #print("slepping 60")
+    #time.sleep(60)
+    #print("continue")
+
+    for l in response['result']['targetInfos']:
+        print(l['targetId'] + " | " + l['type']  + ' | ' + l['title'] + ' | ' + l['url'])
+
+asyncio.get_event_loop().run_until_complete(main())
