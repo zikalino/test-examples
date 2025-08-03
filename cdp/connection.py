79d2ee9cde1d16ad73b1ea38e63c43a81e1886e4
Zim Kalinowski <zkalinowski@opera.com>
2025-08-03 15:48:14 +0800

    simplified

---
+++ cdp/connection.py
@@ -0,0 +1,43 @@
+## demo.py
+
+import asyncio
+import websockets
+import json
+import time
+import sys
+
+from pyppeteer import launch
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from settings import EXECUTABLE_LOCATION
+
+next_id = 1
+websocket = None
+
+async def launch_and_connect():
+  global websocket
+  # path to extension to be loaded
+  extension_path = str(Path(__file__).resolve().parent.joinpath('extension'))
+  print(extension_path)
+
+  browser = await launch({'executablePath': EXECUTABLE_LOCATION,
+                          'ignoreDefaultArgs': True,
+                          'args': ['--load-extension=' + extension_path]})
+
+  return await websockets.connect(browser.wsEndpoint)
+  
+async def send(ws, msg):
+    await ws.send(json.dumps(msg))
+
+async def receive_response(ws, msg):
+    response = {}
+    while (not 'id' in response) or (response['id'] != msg['id']):
+        response = json.loads(await ws.recv())
+        print(f" ... received: ")
+        print(response)
+    return response
+
+def get_next_id():
+  global next_id
+  next_id += 1
+  return next_id
--- cdp/load_extension_and_enumerate.py
+++ cdp/load_extension_and_enumerate.py
@@ -5,12 +5,8 @@ import websockets
 import json
 import time
 from pyppeteer import launch
-from pathlib import Path
-import sys
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-from settings import EXECUTABLE_LOCATION
 
-next_id = 1
+from connection import launch_and_connect, send, receive_response, get_next_id
 
 async def send_and_wait_for_response(websocket, session_id, method, params):
     global next_id
@@ -22,7 +18,7 @@ async def send_and_wait_for_response(websocket, session_id, method, params):
     if session_id:
         message['sessionId'] = session_id
 
-    await websocket.send(json.dumps(message))
+    await websocket.send(message)
 
     # and await response, just dump all messages without appropriate id
     response = {}
@@ -35,34 +31,22 @@ async def send_and_wait_for_response(websocket, session_id, method, params):
     return response
 
 async def main():
-  global next_id
-  
-  # path to extension to be loaded
-  extension_path = str(Path(__file__).resolve().parent.joinpath('extension'))
-  print(extension_path)
 
-  browser = await launch({'executablePath': EXECUTABLE_LOCATION,
-                          'ignoreDefaultArgs': True,
-                          'args': ['--load-extension=' + extension_path]})
+  ws = await launch_and_connect()
 
-  async with websockets.connect(browser.wsEndpoint) as websocket:
-    print("CONNECTED....")
+  #response = await send_and_wait_for_response(
+  #    websocket,
+  #    None,
+  #    'Extensions.loadUnpacked',
+  #    {
+  #        'path': extension_path
+  #    }
+  #)
 
-    #response = await send_and_wait_for_response(
-    #    websocket,
-    #    None,
-    #    'Extensions.loadUnpacked',
-    #    {
-    #        'path': extension_path
-    #    }
-    #)
-
-    for i in range(10):
-        response = await send_and_wait_for_response(
-                websocket,
-                None,
-                'Target.getTargets',
-                {
+  for i in range(10):
+    msg = { 'id': get_next_id(),
+            'method': 'Target.getTargets',
+            'params': {
                     'filter': [
                         {
                         "exclude": False,
@@ -76,17 +60,15 @@ async def main():
                             "exclude": False
                         }
                     ]
-                })
+                }
+            }
+    await send(ws, msg)
+    response = await receive_response(ws, msg)
 
-        #print("slepping 60")
-        #time.sleep(60)
-        #print("continue")
-
-        print('----------------------------- ' + str(len(response['result']['targetInfos'])))
-        for l in response['result']['targetInfos']:
-            if 'koi' in l['url']:
-                print(l['targetId'] + " | " + l['type']  + ' | ' + l['title'] + ' | ' + l['url'])
-        time.sleep(5)
-    
+    print('----------------------------- ' + str(len(response['result']['targetInfos'])))
+    for l in response['result']['targetInfos']:
+        if 'koi' in l['url']:
+            print(l['targetId'] + " | " + l['type']  + ' | ' + l['title'] + ' | ' + l['url'])
+    time.sleep(5)
 
 asyncio.get_event_loop().run_until_complete(main())
