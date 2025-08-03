0ac0371285eaa23fd8f43640f836cf51633841be
Zim Kalinowski <zkalinowski@opera.com>
2025-08-03 15:55:42 +0800

    simplified system

--- cdp/system.py
+++ cdp/system.py
@@ -1,63 +1,24 @@
 ## demo.py
 
 import asyncio
-import websockets
-import json
-import time
-from pyppeteer import launch
-from pathlib import Path
-import sys
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-from settings import EXECUTABLE_LOCATION
-
-next_id = 1
-
-async def send_and_wait_for_response(websocket, session_id, method, params):
-    global next_id
-    # send message
-    message = { 'id': next_id,
-                'method': method,
-                'params': params
-              }
-    if session_id:
-        message['sessionId'] = session_id
-
-    await websocket.send(json.dumps(message))
-
-    # and await response, just dump all messages without appropriate id
-    response = {}
-    while (not 'id' in response) or (response['id'] != next_id):
-        response = json.loads(await websocket.recv())
-        print(f" ... received: ")
-        print(response)
-
-    next_id += 1
-    return response
+from connection import launch_and_connect, send, receive_response, get_next_id
 
 async def main():
-  global next_id
-  extension_path = str(Path(__file__).resolve().parent.joinpath('extension'))
-  print(extension_path)
 
-  browser = await launch({'executablePath': EXECUTABLE_LOCATION,
-                          'ignoreDefaultArgs': True,
-                          'args': ['--load-extension=' + extension_path]})
+  ws = await launch_and_connect()
 
-  async with websockets.connect(browser.wsEndpoint) as websocket:
+  msg = {
+    'id': get_next_id(),
+    'method': 'SystemInfo.getInfo'
+  }
+  await send (ws, msg)
+  response = await receive_response(ws, msg)
 
-    response = await send_and_wait_for_response(
-        websocket,
-        None,
-        'SystemInfo.getInfo',
-        {}
-    )
-
-    response = await send_and_wait_for_response(
-        websocket,
-        None,
-        'SystemInfo.getProcessInfo',
-        {}
-    )
-
+  msg = {
+    'id': get_next_id(),
+    'method': 'SystemInfo.getProcessInfo'
+  }
+  await send (ws, msg)
+  response = await receive_response(ws, msg)
 
 asyncio.get_event_loop().run_until_complete(main())
