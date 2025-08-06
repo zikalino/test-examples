5cd6cbf0310e8b810d63e1af87ddecf4252d2c37
Zim Kalinowski <zkalinowski@opera.com>
2025-08-06 18:10:50 +0800

    get command line

---
+++ cdp/get_command_line.py
@@ -0,0 +1,18 @@
+import asyncio
+import json
+import time
+from connection import launch_and_connect, send, receive_response, get_next_id
+
+async def main():
+
+  ws = await launch_and_connect()
+
+  msg = {
+    'id': get_next_id(),
+    'method': 'Browser.getBrowserCommandLine'
+  }
+  await send(ws, msg)
+  response = await receive_response(ws, msg)
+  print(json.dumps(response['result'], indent=2))
+
+asyncio.get_event_loop().run_until_complete(main())
