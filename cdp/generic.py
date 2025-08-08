ec5ccd41150a7e4b5dcd9f0ae5db38940263aed5
Zim Kalinowski <zkalinowski@opera.com>
2025-08-08 10:10:27 +0800

    some refactoring + addes sidebar

---
+++ cdp/generic.py
@@ -0,0 +1,52 @@
+from connection import send, receive_response, get_next_id
+
+async def create_and_attach_tab(ws, url):
+  msg = {
+    'id': get_next_id(),
+    'method': 'Target.createTarget',
+    'params': {
+      'url': url
+    }
+  }
+  await send(ws, msg)
+  response = await receive_response(ws, msg)
+
+  target_id = response['result']['targetId']
+
+  return await attach_to_target(ws, target_id)
+
+async def attach_to_target(ws, target_id):
+  msg = {
+    'id': get_next_id(),
+    'method': 'Target.attachToTarget',
+    'params': {
+      'targetId': target_id,
+      'flatten': True
+    }
+  }
+  await send(ws, msg)
+  response = await receive_response(ws, msg)
+  session_id = response['result']['sessionId']
+
+  msg = {
+    'id': get_next_id(),
+    'method': 'Runtime.enable',
+    'sessionId': session_id,
+  }
+  await send(ws, msg)
+  response = await receive_response(ws, msg)
+  return session_id
+
+async def evaluate(ws, session_id, expression):
+  msg = {
+    'id': get_next_id(),
+    'method': 'Runtime.evaluate',
+    'sessionId': session_id,
+    'params': {
+      'expression': expression
+    }
+  }
+
+  await send(ws, msg)
+  response = await receive_response(ws, msg)
+  return response['result']['result']['value']
--- cdp/open_tabs_and_enumerate.py
+++ cdp/open_tabs_and_enumerate.py
@@ -1,57 +1,7 @@
 import asyncio
 import json
-from connection import launch_and_connect, send, receive_response, get_next_id
-
-async def create_and_attach_tab(ws, url):
-  msg = {
-    'id': get_next_id(),
-    'method': 'Target.createTarget',
-    'params': {
-      'url': url
-    }
-  }
-  await send(ws, msg)
-  response = await receive_response(ws, msg)
-
-  target_id = response['result']['targetId']
-
-  return await attach_to_target(ws, target_id)
-
-async def attach_to_target(ws, target_id):
-  msg = {
-    'id': get_next_id(),
-    'method': 'Target.attachToTarget',
-    'params': {
-      'targetId': target_id,
-      'flatten': True
-    }
-  }
-  await send(ws, msg)
-  response = await receive_response(ws, msg)
-  session_id = response['result']['sessionId']
-
-  msg = {
-    'id': get_next_id(),
-    'method': 'Runtime.enable',
-    'sessionId': session_id,
-  }
-  await send(ws, msg)
-  response = await receive_response(ws, msg)
-  return session_id
-
-async def evaluate(ws, session_id, expression):
-  msg = {
-    'id': get_next_id(),
-    'method': 'Runtime.evaluate',
-    'sessionId': session_id,
-    'params': {
-      'expression': expression
-    }
-  }
-
-  await send(ws, msg)
-  response = await receive_response(ws, msg)
-  return response['result']['result']['value']
+from connection import launch_and_connect
+from generic import create_and_attach_tab, evaluate
 
 async def main():
 
@@ -62,7 +12,6 @@ async def main():
   result = await evaluate(ws, session_id, "'Result is ' + 'OK'")
 
   print(json.dumps(result, indent=2))
-
 
 
 asyncio.get_event_loop().run_until_complete(main())
