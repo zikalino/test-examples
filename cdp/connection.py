999f3e340a22b5bf7fd52cefffe53403f48c0448
Zim Kalinowski <zkalinowski@opera.com>
2025-08-21 00:02:56 +0800

    added additional args

--- cdp/connection.py
+++ cdp/connection.py
@@ -9,23 +9,21 @@ import sys
 from pyppeteer import launch
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
+
 from settings import EXECUTABLE_LOCATION
 
 next_id = 1
 websocket = None
 
-async def launch_and_connect(user_dir=None):
+async def launch_and_connect(user_dir=None,
+                             additional_args={}):
 
   global websocket
-  # path to extension to be loaded
-  extension_path = str(Path(__file__).resolve().parent.joinpath('extension'))
 
   args = {'executablePath': EXECUTABLE_LOCATION,
-          'ignoreDefaultArgs': True,
-          'args': [  #'--load-extension=' + extension_path,
-          '--enable-automation',
-          '--enable-unsafe-extension-debugging'
-          ]}
+          'ignoreDefaultArgs': True
+          } | additional_args
+  
 
   if user_dir:
     args['args'].append('--user-data-dir=' + user_dir)
@@ -41,9 +39,28 @@ async def send(ws, msg):
 async def receive_response(ws, msg):
     response = {}
     while (not 'id' in response) or (response['id'] != msg['id']):
+
+      if 'method' in response:
+        if response['method'] == 'Runtime.consoleAPICalled':
+          process_log_message(response)
         # XXX - store if response was unrelated
-        response = json.loads(await ws.recv())
+        #if json.dumps(response) != '{}':
+        #  print('### ' + json.dumps(response))
+      response = json.loads(await ws.recv())
     return response
+
+def process_log_message(msg):
+  if msg['params']['type'] == 'log':
+    args = msg['params']['args']
+    for a in args:
+      print("## " + a['value'])
+
+async def get_message(ws):
+  while True:
+    msg = json.loads(await ws.recv())
+    if 'method' in msg:
+      if msg['method'] == 'Runtime.consoleAPICalled':
+        process_log_message(msg)
 
 def get_next_id():
   global next_id
